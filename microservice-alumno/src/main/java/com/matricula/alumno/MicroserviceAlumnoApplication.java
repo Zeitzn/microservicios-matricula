4640910f338f8e8494d70d823fd69edb58af4cd5
zeitzn <smevy4lc3@gmail.com>
2021-06-19 22:28:31 -0500

    gateway config

--- microservice-alumno/src/main/java/com/matricula/alumno/MicroserviceAlumnoApplication.java
+++ microservice-alumno/src/main/java/com/matricula/alumno/MicroserviceAlumnoApplication.java
@@ -3,11 +3,13 @@ package com.matricula.alumno;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @OpenAPIDefinition(info=@Info(title = "Microservicio Alumno",version = "1.0",description = "Microservicio para la administración de estudiantes"))
 public class MicroserviceAlumnoApplication {
 
--- microservice-curso/src/main/java/com/matricula/curso/MicroserviceCursoApplication.java
+++ microservice-curso/src/main/java/com/matricula/curso/MicroserviceCursoApplication.java
@@ -2,12 +2,14 @@ package com.matricula.curso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 @OpenAPIDefinition(
 		info=@Info(
